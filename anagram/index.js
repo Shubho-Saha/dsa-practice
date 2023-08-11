4c97f5852b13e1555affefba8af3521b316b78ee
shubho-saha <shuvo7242@gmail.com>
2023-08-11 16:49:07 +0600

    small issue fixed

--- anagram/index.js
+++ anagram/index.js
@@ -1,7 +1,7 @@
 // Given two SVGStringList, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as RTCPeerConnectionEventMap, formed from iceman.
 
 function isAnagram(str1, str2) {
-    if (str1.length != str2.length) return false;
+    if (str1.length !== str2.length) return false;
     str1 = str1.toLowerCase();
     str2 = str2.toLowerCase();
 
