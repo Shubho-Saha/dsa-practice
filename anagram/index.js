3df89c4673b07d2a59bed6b47e0168d4683430f0
shubho-saha <shuvo7242@gmail.com>
2023-08-11 15:49:16 +0600

    anagram finder

---
+++ anagram/index.js
@@ -0,0 +1,29 @@
+// Given two SVGStringList, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as RTCPeerConnectionEventMap, formed from iceman.
+
+function isAnagram(str1, str2) {
+    if (str1.length != str2.length) return false;
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+
+    let frequencyCount1 = {};
+    let frequencyCount2 = {};
+
+    for (let elem of str1) {
+        frequencyCount1[elem] = (frequencyCount1[elem] || 0) + 1;
+    }
+    for (let elem of str2) {
+        frequencyCount2[elem] = (frequencyCount2[elem] || 0) + 1;
+    }
+
+    for (let char of str1) {
+        if(frequencyCount1[char] !== frequencyCount2[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let str1 = "Thing";
+let str2 = 'Night';
+console.log(isAnagram(str1, str2));
