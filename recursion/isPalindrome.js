a862a00aed17b3e084da65c7c46e806ad98c07b8
shubho-saha <shuvo7242@gmail.com>
2023-08-22 00:39:58 +0600

    recursion 4 more problem added

---
+++ recursion/isPalindrome.js
@@ -0,0 +1,14 @@
+// isPalindrome
+// Write a recursive function called isPalindrome which returns true
+// if the string passed to it is a palindrome (reads the same forward and backward).
+// Otherwise it returns false.
+
+function isPalindrome(str) {
+    if (!str.length) return true;
+
+    if (str[0] !== str[str.length-1]) return false;
+
+    return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome('oos'));
---
+++ recursion/reverse.js
@@ -0,0 +1,12 @@
+// reverse
+// Write a recursive function called reverse
+// which accepts a string and returns a new string in reverse.
+
+function reverse(str) {
+    if (!str.length) return "";
+
+    return str[str.length - 1] + reverse(str.slice(0, str.length-1));
+}
+
+console.log(reverse('shuvo'));
+console.log(reverse('mr'));
