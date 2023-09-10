abd77f7c1dc0600190719f196af1079ce347b488
shubho-saha <shuvo7242@gmail.com>
2023-09-10 23:46:46 +0600

    DLL get and set method

--- data-structures/doubly-linked-list/doubly-linked-list.js
+++ data-structures/doubly-linked-list/doubly-linked-list.js
@@ -71,7 +71,39 @@ class DoublyLinkedList {
         return this;
     }
 
-   
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        let count;
+        let current;
+        if (index <= this.length/2) {
+            count = 0;
+            current = this.head;
+            while(count !== index) {
+                current = current.next;
+                count++
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+         
+    }
+
+    set(index, value) {
+        let targetNode = this.get(index);
+        if (targetNode) {
+            let newNode = new Node(value);
+            targetNode.val = newNode.val;
+            return true;
+        } 
+        return false;
+
+    }
     print() {
         let values = [];
         let current = this.head;
