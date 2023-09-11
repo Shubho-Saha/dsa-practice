4f4b73cb4f9d370aef76e36c9102670e9dbd2a31
shubho-saha <shuvo7242@gmail.com>
2023-09-11 23:39:54 +0600

    DLL insert method added

--- data-structures/doubly-linked-list/doubly-linked-list.js
+++ data-structures/doubly-linked-list/doubly-linked-list.js
@@ -37,7 +37,7 @@ class DoublyLinkedList {
             this.tail = temp.prev;
             this.tail.next = null;
             temp.prev = null;
-        }  
+        }
         this.length--;
         return temp.val;
     }
@@ -48,7 +48,7 @@ class DoublyLinkedList {
         if (this.length === 1) {
             this.head = null;
             this.tail = null;
-        } else {  
+        } else {
             this.head = oldHead.next;
             this.head.prev = null;
             oldHead.next = null;
@@ -75,35 +75,55 @@ class DoublyLinkedList {
         if (index < 0 || index >= this.length) return undefined;
         let count;
         let current;
-        if (index <= this.length/2) {
+        if (index <= this.length / 2) {
             count = 0;
             current = this.head;
-            while(count !== index) {
+            while (count !== index) {
                 current = current.next;
                 count++
             }
         } else {
             count = this.length - 1;
             current = this.tail;
-            while(count !== index) {
+            while (count !== index) {
                 current = current.prev;
                 count--;
             }
         }
         return current;
-         
+
     }
 
     set(index, value) {
         let targetNode = this.get(index);
         if (targetNode) {
-            let newNode = new Node(value);
-            targetNode.val = newNode.val;
+            targetNode.val = value;
             return true;
-        } 
+        }
         return false;
 
     }
+
+    insert(index, value) {
+        if (index < 0 || index > this.length) return false;
+
+        if (index === 0) return !!this.unshift(value);
+        if (index === this.length) return !!this.push(value);
+
+        let newNode = new Node(value);
+        let leftNode = this.get(index - 1);
+        let rightNode = leftNode.next;
+
+        leftNode.next = newNode;
+        newNode.prev = leftNode;
+        rightNode.prev = newNode;
+        newNode.next = rightNode;
+        
+        this.length++;
+        return true;
+
+    }
+
     print() {
         let values = [];
         let current = this.head;
