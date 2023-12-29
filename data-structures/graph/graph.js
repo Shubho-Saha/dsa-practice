3c3d58d669bab9e47e0dbded7221db76c8488ef1
shubho-saha <shuvo7242@gmail.com>
2023-12-29 19:38:21 +0600

    graph added

---
+++ data-structures/graph/graph.js
@@ -0,0 +1,43 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    }
+
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+        
+    }
+}
+
+let graph = new Graph();
+graph.addVertex("dhaka");
+graph.addVertex("sylhet");
+graph.addVertex('Ctg');
+graph.addVertex('Mymensingh');
+
+graph.addEdge('dhaka', 'sylhet');
+graph.addEdge('dhaka', 'Ctg');
+graph.addEdge('Mymensingh', 'Ctg');
+
+console.log(graph.adjacencyList);
+graph.removeVertex('Mymensingh');
+
