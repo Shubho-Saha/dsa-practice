0d4d04db6fbdb2133297ee60a750c59b981e4f29
shubho-saha <shuvo7242@gmail.com>
2024-02-02 16:24:38 +0600

    count unique value optimized

--- problem-solving-pattern/multiple-pointers/countUniqueValue.js
+++ problem-solving-pattern/multiple-pointers/countUniqueValue.js
@@ -1,20 +1,18 @@
 // Implement a function called countUniqueValues, which accepts a sorted Array, and counts the unique values in the Array. There can be negative numbers in the Array, but it will always be sorted.
 
 function countUniqueValues(arr) {
-    let count=0;
+    if (!arr.length) return "invalid input";
+    let count = 1;
 
-    let i = 0;
+    let i = 1;
     while (i < arr.length) {
-        console.log('inside while loop')
-        if (!(arr[i] == arr[i+1])) {
-            console.log(arr[i], arr[i+1]);
+        if (!(arr[i] == arr[i - 1])) {
+            console.log(arr[i], arr[i - 1]);
             count++;
         }
         i++;
     }
-    
     return count;
-    
 }
 
-console.log(countUniqueValues([1, 1, 2]));
+console.log(countUniqueValues([1, 2, 2, 3, 4, 5, 6, 6]));
